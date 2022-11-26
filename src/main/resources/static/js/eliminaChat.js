7611996586c5b781f078d27d9bd55542a5c05dba
Francesco Maselli <francescomaselli41@gmail.com>
2022-11-26 17:06:48 +0100

    indentato il codice in alcuni file js

--- src/main/resources/static/js/eliminaChat.js
+++ src/main/resources/static/js/eliminaChat.js
@@ -1,14 +1,14 @@
- function deleteChat(idUtenteConCuiAbbiamoChattato){
- let formData = new FormData();
- formData.append('idUtenteConCuiAbbiamoChattato',idUtenteConCuiAbbiamoChattato);
- let request = new XMLHttpRequest();
- let url = 'http://localhost:8081/eliminaChat';
- request.open('DELETE',url);
- request.send(formData);
-  
- request.onreadystatechange = function() {
- if (this.readyState == 4 && this.status == 200) {
- window.location.reload();
- }
- };
- }
+function deleteChat(idUtenteConCuiAbbiamoChattato) {
+	let formData = new FormData();
+	formData.append('idUtenteConCuiAbbiamoChattato', idUtenteConCuiAbbiamoChattato);
+	let request = new XMLHttpRequest();
+	let url = 'http://localhost:8081/eliminaChat';
+	request.open('DELETE', url);
+	request.send(formData);
+
+	request.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			window.location.reload();
+		}
+	};
+}
